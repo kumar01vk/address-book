9bc7ae05d027804445fac622a76c4ec429381fda
kumar01vk <121853671+kumar01vk@users.noreply.github.com>
2023-07-12 02:02:51 +0530

    Address Book of Users

---
+++ address-book/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import AddressBook from './components/AddressBook';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AddressBook />
+    </div>
+  );
+}
+
+export default App;
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Contact.create(name: "Vivek", contact_number: "12345678", address: "Mumbai", gender: "Male", age: "23")
+Contact.create(name: "Pavan", contact_number: "233456342", address: "Amravati", gender: "Male", age: "27")
